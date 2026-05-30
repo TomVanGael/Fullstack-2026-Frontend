2134b2f86e4f39d151b5727e6fbada7902884637
=Eddy Mouws <=>
2026-05-30 09:17:56 +0200

    Created debug function in javascript

--- 2026-Fullstack-Frontend-Starting-Files/student4/js/student4.js
+++ 2026-Fullstack-Frontend-Starting-Files/student4/js/student4.js
@@ -2,16 +2,24 @@
   * Javascript to get dynamic data from database of our fitness
 */
 
+// If you need debug-information on the browser-console; change "DEBUG" to true, otherwise set it to false
+const debugging = true;
+
 window.onload = updateLessons();
 
+function debug(message) {
+    if (debugging) {
+        console.log(message);
+    }
+}
 function increaseAttendants(attendantsId, actualAttendants, maxAttendants) {
-    console.log("AttendantsId: " + attendantsId);
-    console.log("Actual attendants: " + actualAttendants);
-    console.log("Maximum attendants: " + maxAttendants)
+    debug("AttendantsId: " + attendantsId);
+    debug("Actual attendants: " + actualAttendants);
+    debug("Maximum attendants: " + maxAttendants)
 
     // Check planning and grouplessons for maximum attendants
     actualAttendants += 1;
-    console.log("New actual attendants: " + actualAttendants);
+    debug("New actual attendants: " + actualAttendants);
 
     const now = new Date();
     let lessonDate = now.getFullYear() + "-" + formatTime(now.getMonth() + 1) + "-" + formatTime(now.getDate());
@@ -54,13 +62,14 @@ async function updateLessons() {
     // Get the weekday
     const now = new Date();
     let weekDay = now.getDay();
-    console.log ("Weekday: " + weekDay);
 
     // Get the time, use formatTime to create leading zeros
     let hours = formatTime(now.getHours());
     let minutes = formatTime(now.getMinutes());
     let time = `${hours}:${minutes}`;
-    console.log("Current Time: " + time);
+
+    debug("Weekday: " + weekDay);
+    debug("Current Time: " + time);
 
     // Call the API to retrieve data from database
     const response =  await fetch("https://devops-project-backend-zy1h.onrender.com/actual_lessons?weekday=" + weekDay + "&time=" + time);
@@ -70,19 +79,21 @@ async function updateLessons() {
     // Get the table element to add the lessons to
     let table = document.getElementById("table-busy");
 
-    // Loop through the received data and add the lessons to the table
+    /*
+    Loop through the received data and add the lessons to the table
 
-    // Structure / example of output:
-    //     <tr>
-    //         <td>Gym</td>
-    //         <td>
-    //             <meter className="s4-bustle-meter" value="78" min="0" max="100" low="50" high="80" optimum="20"
-    //                    title="5min wachttijd"></meter>
-    //         </td>
-    //     </tr>
+     Structure / example of output:
+         <tr>
+             <td>Gym</td>
+             <td>
+                 <meter className="s4-bustle-meter" value="78" min="0" max="100" low="50" high="80" optimum="20"
+                        title="5min wachttijd"></meter>
+             </td>
+         </tr>
+    */
 
     for (let lesson of actualLessons) {
-        // console.log(lesson);
+        debug(lesson);
 
         // Build the DOM elements to show the lessons and the busy-ness of the location
         let row = table.insertRow(1);
@@ -93,25 +104,32 @@ async function updateLessons() {
 
         // Get actual attendants for a lesson and calculate the percentage of max attendants to show in the meter
         const lessonDate = now.getFullYear() + "-" + formatTime(now.getMonth() + 1) + "-" + formatTime(now.getDate());
-        console.log("LessonDate: " + lessonDate);
+        debug("LessonDate: " + lessonDate);
+
+        // Retreive data from backend
         const response = await fetch("https://devops-project-backend-zy1h.onrender.com/actual_attendants?planningid=" + lesson[6] + "&lessondate=" + lessonDate);
         const data = await response.json();
         let attendantInformation = data.attendants[0];
 
         let actualAttendants = attendantInformation[0];
         let actualAttendantsId = attendantInformation[1];
-        console.log("Actual attendants: " + actualAttendants);
+        debug("Actual attendants: " + actualAttendants);
 
         // Meter is configured from 0 to 100; so we've to calculate the amount of people as a percentage of max attendants...
-        let maxAttendants = lesson[2];  // this is 100% of the capacity
-        let percAttendants = (actualAttendants / maxAttendants) * 100;
-        cell2.innerHTML = "<meter id='mtr-" + actualAttendantsId + "' class='s4-bustle-meter' value='" + percAttendants + "' min='0' max='100' low='50' high='80' optimum='20' title='" + actualAttendants + " van de maximaal " + maxAttendants + "'></meter>";
+        // let maxAttendants = lesson[2];  // this is 100% of the capacity
+        // let percAttendants = (actualAttendants / maxAttendants) * 100;
+        // cell2.innerHTML = "<meter id='mtr-" + actualAttendantsId + "' class='s4-bustle-meter' value='" + percAttendants + "' min='0' max='100' low='50' high='80' optimum='20' title='" + actualAttendants + " van de maximaal " + maxAttendants + "'></meter>";
+
+        // let maxAttendants = lesson[2];  // this is 100% of the capacity
+        // let percAttendants = (actualAttendants / maxAttendants) * 100;
+        // Line below has same functionality but with less code and less variables, we can directly calculate the percentage in the value attribute of the meter and directly
+        cell2.innerHTML = "<meter id='mtr-" + actualAttendantsId + "' class='s4-bustle-meter' value='" + ((actualAttendants / lesson[2]) * 100) + "' min='0' max='100' low='50' high='80' optimum='20' title='" + actualAttendants + " van de maximaal " + lesson[2] + "'></meter>";
 
         // Cell 3 contains a button to indicate you're coming to the lesson, so the attendants can be counted in the database and the meter can be updated for other users
-        if (actualAttendants >= maxAttendants) {
+        if (actualAttendants >= lesson[2]) {
             cell3.innerHTML = "<button disabled class='btn-accent'>Volzet</buttona>"
         } else {
-            cell3.innerHTML = "<button id='btn-" + actualAttendantsId + "' onclick='increaseAttendants(" + actualAttendantsId + "," + actualAttendants + "," +  maxAttendants + ")' class='btn-accent'>Ik kom!</buttona>"
+            cell3.innerHTML = "<button id='btn-" + actualAttendantsId + "' onclick='increaseAttendants(" + actualAttendantsId + "," + actualAttendants + "," +  lesson[2] + ")' class='btn-accent'>Ik kom!</buttona>"
         }
     }
 }
